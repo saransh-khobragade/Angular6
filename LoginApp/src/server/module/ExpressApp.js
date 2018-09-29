97bf262c023ced23762316753cd9796b43daaf51
Ankita173 <gupta.nt28@gmail.com>
2018-09-29 22:37:29 +0530

    restructered router

--- LoginApp/src/server/module/ExpressApp.js
+++ LoginApp/src/server/module/ExpressApp.js
@@ -1,54 +1,23 @@
 const express = require('express')
-var router = express.Router();
 const bodyparser = require('body-parser')
 const session = require('express-session')
 var interceptor = require('express-interceptor')
 const connection = require('./Connection')
-const userAPI = require('../service/UserAPI')
-const adminAPI = require('../service/AdminAPI')
-const notificationAPI = require('../service/NotificationAPI')
-const friendAPI = require('../service/FriendAPI')
-const upload = require('../module/Uploads')
+const router = require('./Router')
 const app = express()
-
 
 var preInterceptor = interceptor(function(req, res){
 	//if(!connection.mongoose)
 	//	return res.json({success: false, message:'Mongodb is down'})
 })
 //app.use(preInterceptor);
+
 app.use(session({
     secret: 'ksjldklahshjsljksjkxshjchosjckspcgusjvghhdafhjsbjknsldjl',
     saveUninitialized:true,
     resave:false
 }))
+
 app.use(bodyparser.json())
-
-router.post('/api/login',userAPI.login)	//log in user
-router.get('/api/isUserLoggedIn', userAPI.isUserLoggedIn) //checking logged in user
-router.delete('/api/logout', userAPI.logout)	//log out user
-
-router.get('/api/profile', userAPI.profile)	// ?? dont know
-
-router.post('/api/user', userAPI.createUser)	// register API (create new user)
-router.get('/api/user', userAPI.getOneUser)	// get user with email
-router.put('/api/user', userAPI.editUser)		// edit user details
-router.delete('/api/user', userAPI.deleteOneUser)	//  delete user
-router.get('/api/isUserExist', userAPI.isUserExist) // check for existing user
-
-router.get('/api/allUsers', adminAPI.getAllUsers) // get all users
-router.delete('/api/allUsers', adminAPI.deleteAllUser) //delete all users
-
-router.post('/api/profile/image', upload.single('profilePicture'), userAPI.uploadProfilePic) //upload user profile picture
-
-router.post('/api/friend', friendAPI.invite)	//send friend request --> create an entry in user.invite
-router.post('/api/friend/reject', friendAPI.reject)	//reject friend request --> delete the entry from notification
-router.post('/api/friend/accept', friendAPI.accept) 	// accept friend request --> delete entry from notification and add email to user.friends
-router.delete('/api/friend', friendAPI.unFriend)	// unfriend -->  delete email from user.friends
-router.get('/api/friend/all', friendAPI.getAllFriends)	// get all friends --> return user.friends
-router.get('/api/friend/recommend', friendAPI.getRecommendedFriends) //return users-friends
-
-router.post('/api/notification', notificationAPI.openNotification)	//(remove) open notification --> push user.invite to notification, return user.invite
-
 app.use(router);
 module.exports = app

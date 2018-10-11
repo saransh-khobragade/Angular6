3b8cecef438d04c155abb90903ac96b26fb5cc7e
Ankita173 <42465869+Ankita173@users.noreply.github.com>
2018-10-11 20:49:29 +0530

    enhanced recommended friends api

--- LoginApp/src/server/service/FriendAPI.js
+++ LoginApp/src/server/service/FriendAPI.js
@@ -1,24 +1,30 @@
 const User = require('../model/Users')
 const Notification = require('../model/Notification')
-//const lodash = require('lodash')
+const lodash = require('lodash')
+
+function userModel(fname, email, id) {
+    this.fname = fname;
+    this.email = email;
+    this.id = id;
+}
 
 exports.invite = async (req, res) => {
 	const{email,friend}=req.body
 	console.log(req.session[email])
 	if(!req.session[email])
 		 return res.json({success: false, message:'User not logged in'})
-//	User.findOne({email}, function(err, user){
-//		if (err) {  
-//				return res.json({success: false, message:'Something went wrong'})  
-//			} 
-//		console.log(user.friends)
-//		if(user.friends){
-//			console.log(user.friends)
-//			console.log(lodash.filter(user.friends, friend))
-//			if(user.friends.find(friend))
-//				return res.json({success: false, message:'Already friends'})
-//		}
-//	})
+	User.findOne({email}, function(err, user){
+		if (err) {  
+				return res.json({success: false, message:'Something went wrong'})  
+		} 
+		console.log(user.friends)
+		if(user.friends.length != 0){
+			console.log(lodash.filter(user.friends, x => x === friend));
+			//console.log(lodash.filter(user.friends, friend))
+			if(user.friends.find(friend))
+				return res.json({success: false, message:'Already friends'})
+		}
+	})
 	
 	const entry = new Notification({type: 'invite', creater: email, receiver: friend})
 	entry.save(function (err) {  
@@ -64,6 +70,10 @@ exports.accept = async (req, res) => {
 exports.unFriend = async (req, res) => {
 	const email = req.query.email
 	const friend = req.query.friend
+	if(email == null || email == "")
+		return res.json({success: false, message:'Please send email as query param'})
+	if(friend == null || friend == "")
+		return res.json({success: false, message:'Please send friend as query param'})
 	 User.updateOne({ email }, { $pull: { friends: friend } }, function(err){
 		if (err) {  
             return res.json({success: false, message:'Something went wrong1'})  
@@ -78,26 +88,37 @@ exports.unFriend = async (req, res) => {
 };
 
 exports.getAllFriends = async (req, res) => {
-	User.findOne({email: req.query.email}, function(err, user){
+	const email = req.query.email
+	if(email == null || email == "")
+		return res.json({success: false, message:'Please send email as query param'})  
+	User.findOne({email}, function(err, user){
 		if (err) {  
 				return res.json({success: false, message:'Something went wrong'})  
 			} 
 		return res.json(user.friends)
 	})
 };
+function findUnCommon(data,data2){
+    var filter = [];
+   for(var i=0; i<data.length; i++)
+       if(data2.indexOf(data[i].email)<0)
+            filter.push(data[i]);
+   return filter;
+}
 
 exports.getRecommendedFriends = async (req, res) => {
 	const email = req.query.email
-	if(email == null)
+	if(email == null || email == "")
 		return res.json({success: false, message:'Please send email as query param'})  
 	var users = [];
 	var friends = [];
+
 	User.find().stream()
 		.on('data', function(user){
-			users.push(user.email) 
+			users.push(new userModel(user.fname, user.email, user._id))
 		})
 		.on('error', function(err){
-			return res.json({success: false, message:'Something went wrong'})  
+			res.json({success: false, message:'Something went wrong'})  
 		})
 		.on('end', function(){
 			User.findOne({email}, function(err, user){
@@ -111,14 +132,13 @@ exports.getRecommendedFriends = async (req, res) => {
 			})
 			Notification.find({creater:email}).stream()
 				.on('data', function(notf){
-					console.log(notf.receiver)
 					friends.push(notf.receiver) 
 				})
 				.on('error', function(err){
-					return res.json({success: false, message:'Something went wrong'})  
+					res.json({success: false, message:'Something went wrong'})  
 				})
 				.on('end', function(){
-					return res.json(users.filter(x => !friends.includes(x)));
+						res.json(findUnCommon(users,friends))
 				})
 		});
 };

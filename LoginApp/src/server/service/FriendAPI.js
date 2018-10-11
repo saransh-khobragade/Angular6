32a9b6504f07660869a4eee6a725a3b7ed923f5c
Ankita173 <gupta.nt28@gmail.com>
2018-10-11 21:56:42 +0530

    updated chnages

--- LoginApp/src/server/service/FriendAPI.js
+++ LoginApp/src/server/service/FriendAPI.js
@@ -1,24 +1,27 @@
 const User = require('../model/Users')
 const Notification = require('../model/Notification')
-//const lodash = require('lodash')
+const lodash = require('lodash')
+
+function userModel(fname, email, id) {
+    this.fname = fname;
+    this.email = email;
+    this.id = id;
+}
 
 exports.invite = async (req, res) => {
 	const{email,friend}=req.body
 	console.log(req.session[email])
 	if(!req.session[email])
 		 return res.json({success: false, message:'User not logged in'})
-//	User.findOne({email}, function(err, user){
-//		if (err) {  
-//				return res.json({success: false, message:'Something went wrong'})  
-//			} 
-//		console.log(user.friends)
-//		if(user.friends){
-//			console.log(user.friends)
-//			console.log(lodash.filter(user.friends, friend))
-//			if(user.friends.find(friend))
-//				return res.json({success: false, message:'Already friends'})
-//		}
-//	})
+	User.findOne({email}, function(err, user){
+		if (err) {  
+				return res.json({success: false, message:'Something went wrong'})  
+		} 
+		console.log(user.friends)
+		if(user.friends.indexOf(friend)<0){
+				return res.json({success: false, message:'Already friends'})
+		}
+	})
 	
 	const entry = new Notification({type: 'invite', creater: email, receiver: friend})
 	entry.save(function (err) {  
@@ -64,6 +67,10 @@ exports.accept = async (req, res) => {
 exports.unFriend = async (req, res) => {
 	const email = req.query.email
 	const friend = req.query.friend
+	if(email == null || email == "")
+		return res.json({success: false, message:'Please send email as query param'})
+	if(friend == null || friend == "")
+		return res.json({success: false, message:'Please send friend as query param'})
 	 User.updateOne({ email }, { $pull: { friends: friend } }, function(err){
 		if (err) {  
             return res.json({success: false, message:'Something went wrong1'})  
@@ -78,13 +85,30 @@ exports.unFriend = async (req, res) => {
 };
 
 exports.getAllFriends = async (req, res) => {
-	User.findOne({email: req.query.email}, function(err, user){
+	const email = req.query.email
+	var friends = []
+	var users;
+	if(email == null || email == "")
+		return res.json({success: false, message:'Please send email as query param'})  
+	
+	User.findOne({email}, function(err, user){
 		if (err) {  
 				return res.json({success: false, message:'Something went wrong'})  
-			} 
-		return res.json(user.friends)
+		}
+		friends = user.friends
 	})
+	User.find({email: { $in: friends}}, function(err, docs){
+		console.log(docs);
+	});
 };
+
+function findUnCommon(data,data2){
+    var filter = [];
+   for(var i=0; i<data.length; i++)
+       if(data2.indexOf(data[i].email)<0)
+            filter.push(data[i]);
+   return filter;
+}
 
 exports.getRecommendedFriends = async (req, res) => {
 	const email = req.query.email
@@ -92,9 +116,10 @@ exports.getRecommendedFriends = async (req, res) => {
 		return res.json({success: false, message:'Please send email as query param'})  
 	var users = [];
 	var friends = [];
+
 	User.find().stream()
 		.on('data', function(user){
-			users.push(user.email) 
+			users.push(new userModel(user.fname, user.email, user._id))
 		})
 		.on('error', function(err){
 			res.json({success: false, message:'Something went wrong'})  
@@ -111,14 +136,13 @@ exports.getRecommendedFriends = async (req, res) => {
 			})
 			Notification.find({creater:email}).stream()
 				.on('data', function(notf){
-					console.log(notf.receiver)
 					friends.push(notf.receiver) 
 				})
 				.on('error', function(err){
 					res.json({success: false, message:'Something went wrong'})  
 				})
 				.on('end', function(){
-					res.json(users.filter(x => !friends.includes(x)));
+						res.json(findUnCommon(users,friends))
 				})
 		});
 };

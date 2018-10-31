fec8664109dc4b691c494c20cbd987cffaf92ce0
unknown <saru998>
2018-10-31 10:25:37 +0530

    some more fixes

--- SocialNetwork/Nodejs/server/api/notification.js
+++ SocialNetwork/Nodejs/server/api/notification.js
@@ -2,4 +2,23 @@ const User = require('../model/Users')
 const express = require('express')
 const router = express.Router();
 
+
+/* const Notification = require('../model/Notification')
+const db = require('../module/MongoDBMethods')
+const User = require('../model/Users')
+
+exports.inviteNotification =  async (req,res)=>{
+    const email = req.query.email
+    if(email==="") res.json({success: false, message:'Got blank email for invites'})
+
+    let invites= await db.FindWithKeys(Notification,[{type:"invite"},{ "receiver.email": email}])   
+    console.log(invites)
+    
+    let result=[]
+    for(let a of invites){
+        result.push(a.creater)
+    };
+    res.json(result)
+}; */
+
 module.exports = router;
--- SocialNetwork/Nodejs/server/api/user.js
+++ SocialNetwork/Nodejs/server/api/user.js
@@ -19,15 +19,45 @@ router.post('/api/user',async (req, res) =>{
     })
 });
 
-/* router.profile = async (req,res)=>{
+/* exports.login = async (req,res)=>{		//login API
+    const{email,password}=req.body
+	User.findOne({email},(function (err, result) {
+		if (err) {
+			return res.json({success: false, message: 'Something went wrong'})
+		} 
+		else if(!result)
+			return res.json({success:false,message: 'Email id doesn\'t exists'})
+		else{
+			User.findOne({email,password}, function(error, output){
+				if (err) {
+					return res.json({success: false, message: 'Something went wrong'})
+				}
+				else if(!output){
+					return res.json({success: false, message:'Incorrect Password'})
+				} 
+				req.session[email] = output._id
+				req.session.save()
+				
+				res.json({success: true, message:'Login Success'})
+			})
+		}
+	}))  
+};
+
+exports.isUserLoggedIn = async (req,res)=>{
+
+	return res.json({status: req.session[req.query.email]? true : false })
+};
+
+exports.profile = async (req,res)=>{
     const user = await User.findOne({email:req.session[email]})
     if(!user){
         return res.json({success: false, message:'user was deleted'})
     }
     return res.json({success: true, email:req.session[email]})
-}; */
+};
 
-/* router.isUserExist = async (req, res) =>{		
+exports.isUserExist = async (req, res) =>{		
 	User.findOne({email:req.query.email},(function (err, result) {
 		if (err) {
 			return res.json({success: false, message: 'Something went wrong'})
@@ -37,10 +67,25 @@ router.post('/api/user',async (req, res) =>{
 		}
 		return res.json({success:false,message: 'User doesn\'t exists'})
 	}))
-}; */
+};
 
+exports.createUser = async (req, res) =>{		
+	const{fname, lname, email, password, phone, gender, dob} = req.body
+	const result = await User.findOne({email})
+    if(result){
+        return res.json({success: false, message: 'User already exists'})
+    }
 
-/* router.getOneUser = async (req, res)=>{			// get user with id
+	const user = new User({fname, lname, email, password, phone, gender, dob})
+	user.save(function (err) {  
+        if (err) {  
+            return res.json({success: false, message:'Something went wrong'})  
+        }  
+        return res.json({success: true, message:'Registration successful'})  
+    })
+};
+
+exports.getOneUser = async (req, res)=>{			// get user with id
 	User.findOne({email:req.query.email}, function (err, user) {
 		if(err) {
 			return res.json({success: false, message: 'Something went wrong'})
@@ -51,8 +96,8 @@ router.post('/api/user',async (req, res) =>{
 		return res.json(user)		
 	}).select('fname lname email phone gender dob')
 };
- */
-/* router.editUser = async (req, res)=>{			// update user details	
+
+exports.editUser = async (req, res)=>{			// update user details	
 	const email = req.query.email
 	User.findOne({email}, function(err, user) {		
 		if(err){
@@ -66,18 +111,18 @@ router.post('/api/user',async (req, res) =>{
 			return res.json({success: true, message:'User details updated successfully'})
 		})
 	})
-}; */
+};
 
-/* router.deleteOneUser = async (req,res)=>{		//delete user
+exports.deleteOneUser = async (req,res)=>{		//delete user
     User.deleteOne({ email:req.query.email }, function (err) {  
         if (err) {  
             return res.json({success: false, message:'Something went wrong'})  
         }  
         return res.json({success: true, message: 'Successfully deleted' });  
     })  
-}; */
+};
 
-/* router.uploadProfilePic = async (req, res)=> {
+exports.uploadProfilePic = async (req, res)=> {
 	const email = req.session[email]
 	User.findOne({email}, function(err, user) {		
 		if(err){
@@ -93,8 +138,14 @@ router.post('/api/user',async (req, res) =>{
 		return res.json({success: true, message: 'Profile picture saved successfully'})
 		})
 	})
-}; */
+};
+
+exports.logout = async (req,res)=>{
+	delete req.session[req.query.email]
+	return res.json({success:true})
+};
 
 
+ */
 
 module.exports = router;

4780196aa495bcf0881c48017baa4e13825b9a99
unknown <saru998>
2018-11-20 19:52:09 +0530

    status profile pic fixed

--- SocialNetwork/Angular6/src/app/home/board/status/status.component.ts
+++ SocialNetwork/Angular6/src/app/home/board/status/status.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/service/user.service';
 
-interface user {  id:number,  fname: string,  lname: string,  email:string,  phone:number,  gender:string,  dob:string,  profilePic:string}
+interface user { id: number, fname: string, lname: string, email: string, phone: number, gender: string, dob: string, profilePic: string }
 
 @Component({
   selector: 'app-status',
@@ -9,39 +9,49 @@ interface user {  id:number,  fname: string,  lname: string,  email:string,  pho
   styleUrls: ['./status.component.css']
 })
 export class StatusComponent implements OnInit {
-  
-  userAlive:Boolean
-  userDetails:user
 
-  status=[]
+  userAlive: Boolean
+  userDetails: user
 
-  constructor(private user:UserService) {
-    
-    this.user.userDetails.subscribe( data=>{
-      if(data){
-        this.userDetails=data
+  status = []
+
+  constructor(private user: UserService) {
+
+    this.user.userDetails.subscribe(data => {
+      if (data) {
+        this.userDetails = data
       }
-      
+
     })
-   }
+  }
 
   ngOnInit() {
     this.refreshStatus()
   }
 
-  refreshStatus(){
-    this.user.getAllStatus().subscribe(data=>{
-      if(data.body.success)
-        this.status=[]
-        for(let st in data.body.result){
-          this.status.push({name:data.body.result[st].creater.name,status:data.body.result[st].status})
-        }      
+  refreshStatus() {
+    this.user.getAllStatus().subscribe(data => {
+      if (data.body.success)
+        this.status = []
+
+      for (let st in data.body.result) {
+        let userName = data.body.result[st].creater.name
+        let userEmail = data.body.result[st].creater.email
+        let status = data.body.result[st].status 
+
+        this.user.getUser(userEmail).subscribe((data2=>{
+          let profilePic=data2.result.profilePic
+          this.status.push({userName,profilePic, status:status })
+        }))
+
+        
+      }
     })
   }
 
-  setStatus(status){
-    this.user.setStatus(status,this.userDetails.email).subscribe(data=>{
-      if(data.body.success){
+  setStatus(status) {
+    this.user.setStatus(status, this.userDetails.email).subscribe(data => {
+      if (data.body.success) {
         this.refreshStatus()
       }
     })
